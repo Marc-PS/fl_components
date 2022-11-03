9497b4ee68cc005e24e61025df3d02a9a413747f
Marc-PS <marc.perellosapina@gmail.com>
2022-11-03 18:18:45 +0100

    Text Button Theme

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,31 +1,18 @@
-import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import '../widgets/widgets.dart';
 
 class CardScreen extends StatelessWidget {
-   
   const CardScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Card Widget"),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 20 , vertical: 10),
-        children: [
-          Card(
-            child: Column(
-              children: const [
-                ListTile(
-                  leading: Icon(Icons.abc_outlined,color: AppTheme.primary,),
-                  title: Text("Soy un titulo "),
-                  subtitle: Text('Et et labore nostrud qui ipsum adipisicing adipisicing excepteur proident ex excepteur.'),
-                )
-            ]),
-          )
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: const Text("Card Widget"),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: const [CustomCardType1()],
+        ));
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const  Color primary = Colors.indigo;
+  static const Color primary = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: primary,
-    appBarTheme: const AppBarTheme(
-      color: primary,
-      elevation: 0,
-      )
-  );
+      primaryColor: primary,
+      appBarTheme: const AppBarTheme(
+        color: primary,
+        elevation: 0,
+      ),
+      textButtonTheme:
+          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-    primaryColor: primary,
-    appBarTheme: const AppBarTheme(
-      color: primary,
-      elevation: 0,
+      primaryColor: primary,
+      appBarTheme: const AppBarTheme(
+        color: primary,
+        elevation: 0,
       ),
-    scaffoldBackgroundColor: Colors.black
-  );
-
-
-
+      scaffoldBackgroundColor: Colors.black,
+      textButtonTheme:
+          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)));
 }
