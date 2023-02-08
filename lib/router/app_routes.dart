55132068ee4e9da2cdb6060510d462b04cf19cdb
peresapy <marc.perellosapina@gmail.com>
2023-02-08 13:38:40 +0100

    Animated Screen

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -6,18 +6,20 @@ class AppRoutes {
    static const initialRoute = 'home';
 
    static final menuOptions = <MenuOption>[
-     //TODO
-     MenuOption(route: 'home', name: 'Home Screen', screen: HomeScreen(), icon: Icons.home_outlined),
-     MenuOption(route: 'listView1', name: 'List View tipo 1', screen: ListView1Screen(), icon: Icons.view_list_outlined),
-     MenuOption(route: 'listView2', name: 'List View tipo 2', screen: ListView2Screen(), icon: Icons.list_alt_outlined),
-     MenuOption(route: 'card', name: 'Cards', screen: CardScreen(), icon: Icons.chrome_reader_mode_outlined),
-     MenuOption(route: 'alert', name: 'Alerts', screen: AlertScreen(), icon: Icons.add_alert_outlined),
-    MenuOption(route: 'avatar', name: 'Avatar', screen: AvatarScreen(), icon: Icons.person_outline_outlined)
+    
+    //MenuOption(route: 'home', name: 'Home Screen', screen: HomeScreen(), icon: Icons.home_outlined),
+    MenuOption(route: 'listView1', name: 'List View tipo 1', screen: ListView1Screen(), icon: Icons.view_list_outlined),
+    MenuOption(route: 'listView2', name: 'List View tipo 2', screen: ListView2Screen(), icon: Icons.list_alt_outlined),
+    MenuOption(route: 'card', name: 'Cards', screen: CardScreen(), icon: Icons.chrome_reader_mode_outlined),
+    MenuOption(route: 'alert', name: 'Alerts', screen: AlertScreen(), icon: Icons.add_alert_outlined),
+    MenuOption(route: 'avatar', name: 'Avatar', screen: AvatarScreen(), icon: Icons.person_outline_outlined),
+    MenuOption(route: 'animated', name: 'Animated', screen: AnimatedScreen(), icon: Icons.play_circle_outline_rounded)
    ];
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
    
     Map<String, Widget Function(BuildContext)> appRoutes = {};
+    appRoutes.addAll({ 'home' : ( BuildContext context) => const HomeScreen()});
 
     for (final option in menuOptions) {
       appRoutes.addAll({option.route : (BuildContext context) => option.screen});
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -4,3 +4,5 @@ export 'listView1_screen.dart';
 export 'listView2_screen.dart';
 export 'home_screen.dart';
 export 'package:fl_components/screens/avatar_screen.dart';
+export 'package:fl_components/screens/animated_screen.dart';
+
